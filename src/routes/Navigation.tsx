26b44df3c3be05650fe684dba7c7a102439a5c60
unknown <goodluckxdc@gmail.com>
2023-02-18 12:53:31 -0500

    navbar

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,6 +1,12 @@
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  NavLink,
-} from "react-router-dom";
+import { BrowserRouter, Route, Routes, NavLink } from "react-router-dom";
+import Navbar from "../components/Navbar";
+
+export const Navigation = () => {
+  return (
+    <BrowserRouter>
+      <div className="main-layout">
+        <Navbar />
+      </div>
+    </BrowserRouter>
+  );
+};
